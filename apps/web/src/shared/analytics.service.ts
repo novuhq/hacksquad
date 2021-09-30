a898673b47d5507e19b4d71662b545e7f87ec672
Dima Grossman <dima@grossman.io>
2021-09-30 10:19:36 +0300

    Guard without token

--- apps/web/src/shared/analytics.service.ts
+++ apps/web/src/shared/analytics.service.ts
@@ -1,9 +1,13 @@
 import * as mixpanel from 'mixpanel-browser';
 
 export function initAnalytics() {
-  if (process.env.NEXT_PUBLIC_ANALYTICS_ID) mixpanel.init(process.env.NEXT_PUBLIC_ANALYTICS_ID);
+  if (process.env.NEXT_PUBLIC_ANALYTICS_ID) {
+    mixpanel.init(process.env.NEXT_PUBLIC_ANALYTICS_ID);
+  }
 }
 
 export function trackAnalyticsEvent(eventName: string, eventProps?: any): void {
-  if (mixpanel.track) mixpanel.track(eventName, eventProps);
+  if (mixpanel.track && process.env.NEXT_PUBLIC_ANALYTICS_ID) {
+    mixpanel.track(eventName, eventProps);
+  }
 }
